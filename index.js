a5b5be98a532e0254d784dfc5e28df9c38d1da14
Rémi Lebret <remi.lebret@ext.adeoservices.com>
2016-12-10 13:48:17 +0100

    ajout RESTFULL + expressVAlidator

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
+/*eslint no-console: 0*/
 'use strict';
 
-var express = require('express');
-var kraken = require('kraken-js');
+var express = require('express')
+  , expressValidator = require('express-validator')
+  , kraken = require('kraken-js')
+  , bodyParser     = require('body-parser')
+  , methodOverride = require('method-override')
+  ;
 
 
 var options, app;
+
+app = module.exports = express();
+
+/**
+ * Activation du RESTFULL (get, post, put et delete) pour les controllers
+ * NOTE: when using req.body, you must fully parse the request body
+ *       before you call methodOverride() in your middleware stack,
+ *       otherwise req.body will not be populated.
+ */
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride(
+    function(req /*, res*/){
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+            // look in urlencoded POST bodies and delete it
+            var method = req.body._method;
+            delete req.body._method;
+            return method;
+        }
+    }
+));
+
+// expressValidator must be immediately after express.bodyParser()!
+app.use(expressValidator(require('./lib/expressValidatorLib')));
 
 /*
  * Create and configure application. Also exports application instance for use by tests.
@@ -20,7 +48,6 @@ options = {
     }
 };
 
-app = module.exports = express();
 app.use(kraken(options));
 app.on('start', function () {
     console.log('Application ready to serve requests.');
